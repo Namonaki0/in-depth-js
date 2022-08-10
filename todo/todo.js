8f966d079c28ad372eff2de546e367003a982a20
Andre <andreferreira@live.co.uk>
2022-08-10 23:01:13 +0100

    todo mini project finalized

--- todo/todo.js
+++ todo/todo.js
@@ -1,10 +1,11 @@
 const insertTodoForm = document.querySelector(".insert-todo-form");
+const searchTodo = document.querySelector(".search-todo-form input");
 const list = document.querySelector("ul");
 
 //? CREATE TODOS
-const createTodo = (typedTodo) => {
+const createTodo = (todo) => {
   const html = `<li class="inserted-todo">
-            <span>${typedTodo}</span><i class="delete fa-solid fa-trash-can"></i>
+            <span>${todo}</span><i class="delete fa-solid fa-trash-can"></i>
         </li>`;
 
   list.innerHTML += html;
@@ -14,10 +15,10 @@ const createTodo = (typedTodo) => {
 insertTodoForm.addEventListener("submit", (e) => {
   e.preventDefault();
 
-  const typedTodo = insertTodoForm.todo.value.trim();
+  const todo = insertTodoForm.typedTodo.value.trim();
 
-  if (typedTodo) {
-    createTodo(typedTodo);
+  if (todo) {
+    createTodo(todo);
     insertTodoForm.reset();
   }
 });
@@ -27,4 +28,22 @@ list.addEventListener("click", (e) => {
   if (e.target.classList.contains("delete")) {
     e.target.parentElement.remove();
   }
+});
+
+//? FILTER TODOS
+const filteredSearch = (searchedWord) => {
+  Array.from(list.children)
+    .filter((todo) => !todo.textContent.toLowerCase().includes(searchedWord))
+    .forEach((todo) => todo.classList.add("filtered"));
+
+  Array.from(list.children)
+    .filter((todo) => todo.textContent.toLowerCase().includes(searchedWord))
+    .forEach((todo) => todo.classList.remove("filtered"));
+};
+
+//? FILTER INPUT FIELD HANDLER
+searchTodo.addEventListener("keyup", () => {
+  const searchedWord = searchTodo.value.trim().toLowerCase();
+
+  filteredSearch(searchedWord);
 });
