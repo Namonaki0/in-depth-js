33e14d0585041187fb01b9906afa3d3bf7caa032
Andre <andreferreira@live.co.uk>
2022-08-25 11:44:27 +0100

    retrieve data from firebase db

--- databases/records.js
+++ databases/records.js
@@ -1,5 +1,25 @@
+const form = document.querySelector(".record-form");
+const recordInput = form.querySelector(".record-input");
+const ul = document.querySelector("ul");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  console.log(e);
+});
+
+const addToList = (record) => {
+  let time = record.release_date.toDate();
+  let html = `
+    <li>${record.artist}</li>
+    <span>${time}</span>
+  `;
+
+  ul.innerHTML += html;
+};
+
 db.collection("records")
   .get()
   .then((snapshot) => {
-    console.log(snapshot.docs[0]._document.proto.fields.artist.stringValue);
-  });
+    snapshot.docs.forEach((doc) => addToList(doc.data()));
+  })
+  .catch((err) => console.log(err));
