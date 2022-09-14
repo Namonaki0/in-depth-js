6787a0e89fc91517365e601565a886eb2e0680ad
Andre Ferreira <49961945+Namonaki0@users.noreply.github.com>
2022-09-14 23:44:56 +0100

    Add files via upload

--- mini-projects/ui-library/src/index.js
+++ mini-projects/ui-library/src/index.js
@@ -1,8 +1,21 @@
 import "./style.css";
 
 import Tooltip from "./ui-components/tootip";
+import Dropdown from "./ui-components/dropdown";
+import Tabs from "./ui-components/tabs";
 
-// creating a tooltip
+// create a tooltip
 const tooltip = new Tooltip(document.querySelector(".tooltip"));
-
 tooltip.innit();
+
+// create dropdowns
+const dropdowns = document.querySelectorAll(".dropdown");
+
+dropdowns.forEach((dropdown) => {
+  const action = new Dropdown(dropdown);
+  action.init();
+});
+
+// create tabs
+const tabs = new Tabs(document.querySelector(".tabs"));
+tabs.init();
