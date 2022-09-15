b3f8a7cb0c2693d3901f0d190e3f9f3c6de1ab11
Andre Ferreira <49961945+Namonaki0@users.noreply.github.com>
2022-09-15 23:21:51 +0100

    Add files via upload
    snackbar import

--- mini-projects/ui-library/src/index.js
+++ mini-projects/ui-library/src/index.js
@@ -3,6 +3,7 @@ import "./style.css";
 import Tooltip from "./ui-components/tootip";
 import Dropdown from "./ui-components/dropdown";
 import Tabs from "./ui-components/tabs";
+import Snackbar from "./ui-components/snackbar";
 
 // create a tooltip
 const tooltip = new Tooltip(document.querySelector(".tooltip"));
@@ -19,3 +20,11 @@ dropdowns.forEach((dropdown) => {
 // create tabs
 const tabs = new Tabs(document.querySelector(".tabs"));
 tabs.init();
+
+// create snackbar
+const snackbar = new Snackbar();
+snackbar.init();
+
+document.querySelector("button").addEventListener("click", () => {
+  snackbar.show("clicked the button");
+});
